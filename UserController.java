9ebdd6c0d29d3c9362438e53e73c1cee7df92494
xman6565 <xman6565@naver.com>
2020-02-12 17:20:33 +0900

    Add UserContrller [Insert]

--- UserController.java
+++ UserController.java
@@ -2,10 +2,18 @@
 public class UserController{
 
    // 로그인 페이지로 이동
-   @RequestMapping("/user/login/.do")
+   @RequestMapping("/user/login.do")
     public String login(){
         logger.info("경로 : login")
 
        return "user/login"; 
+   }
+
+    // 로그인 페이지로 이동
+   @RequestMapping("/user/insert.do")
+    public String login(){
+        logger.info("경로 : insert")
+
+       return "user/insert"; 
    }
 }

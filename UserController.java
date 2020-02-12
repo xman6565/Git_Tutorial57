5c2cd5533d1f642fdc0c43c6192f10ecb2344e1b
xman6565 <xman6565@naver.com>
2020-02-12 18:00:12 +0900

    Add UserController [Delete]

--- UserController.java
+++ UserController.java
@@ -17,11 +17,19 @@ public class UserController{
        return "user/insert"; 
    }
 
-   // 로그인 페이지로 이동
+   // 회원수정 페이지 페이지로 이동
    @RequestMapping("/user/update.do")
     public String update(){
         logger.info("경로 : update")
 
        return "user/update"; 
+   }
+   
+   // 회원수정 페이지 페이지로 이동
+   @RequestMapping("/user/delete.do")
+    public String delete(){
+        logger.info("경로 : delete")
+
+       return "user/delete"; 
    }
 }
